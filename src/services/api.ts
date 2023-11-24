0949d6ad8ca35c1734e34a61a7cfa6515819b752
Belmarques <gabrielll0701@gmail.com>
2023-11-24 12:52:16 -0300

    exercicio 1-2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,20 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const urlQuer = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const urlCat = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const response = await fetch(urlQuer);
+  const result = await fetch(urlCat);
+  const data = await response.json();
+  const dados = await result.json();
+  return data && dados;
 }
 
 export async function getProductById() {
