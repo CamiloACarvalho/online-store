b943ab1be5cca40399610011c9ea4bcbfa72be5d
Belmarques <gabrielll0701@gmail.com>
2023-11-25 14:31:40 -0300

    corrigingo o 5

--- src/components/home/Index.tsx
+++ src/components/home/Index.tsx
@@ -1,111 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getProductsFromCategoryAndQuery, getCategories } from '../../services/api';
+import Categorie from '../categories/categories';
+import { getProductsFromCategoryAndQuery } from '../../services/api';
 
 function Home() {
   const [search, setSearch] = useState('');
-  const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState('');
   const [products, setProducts] = useState([]);
 
   const getValueInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     const valueInput = event.target.value;
     setSearch(valueInput);
   };
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const result = await getCategories();
-        setCategories(result);
-      } catch (error) {
-        console.error('Erro ao carregar a API', error);
-      }
-    };
-
-    fetchCategories();
-  }, []);
-
-  const getProducts = async () => {
-    try {
-      const resultAPI = await getProductsFromCategoryAndQuery(categoryId, search);
-      if (resultAPI && resultAPI.results) {
-        setProducts(resultAPI.results);
-        setCategoryId(resultAPI.id);
-      } else {
-        console.error('A resposta da API não contém os resultados esperados.', resultAPI);
-      }
-    } catch (error) {
-      console.error('Erro ao buscar produtos', error);
-    }
+  const handleClick = async () => {
+    const response = await getProductsFromCategoryAndQuery(categories, search);
+    setProducts(response.results);
+    setCategories(response.results.id);
+    setSearch('');
   };
-
   return (
     <>
       <div>
-        <h2 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h2>
+        <input
+          data-testid="query-input"
+          type="text"
+          value={ search }
+          onChange={ getValueInput }
+        />
+        <button
+          type="button"
+          onClick={ handleClick }
+          data-testid="query-button"
+        >
+          Buscar
+
+        </button>
         {search === '' && (
-          <input
-            data-testid="query-input"
-            type="text"
-            value={ search }
-            onChange={ getValueInput }
-          />
+          <h2 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h2>
         )}
+        {products.map((product:any) => (
+          <div key={ product.id }>
+            <h2 data-testid="product">
+              {product.title}
+            </h2>
+            {}
+            <img src={ product.thumbnail } alt="imagem" />
+            <h3>
+              {' '}
+              R$
+              {' '}
+              {product.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+
+            </h3>
+          </div>
+        ))}
       </div>
-      <button
-        data-testid="query-button"
-        type="button"
-        onClick={ getProducts }
-      >
-        Buscar
-      </button>
       <Link
         data-testid="shopping-cart-button"
         to="/ShoppingBasket"
       >
         Ir para carrinho de compras
       </Link>
-      <aside>
-        <section>
-          <h2> Categorias </h2>
-          <ul>
-            {categories.map((category:any, index) => (
-              <li
-                key={ index }
-              >
-                <label htmlFor={ `category${index}` }>
-                  <input
-                    id={ `category${index}` }
-                    data-testid="category"
-                    type="radio"
-                  />
-                  { category.name }
-                </label>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </aside>
-      <section>
-        <ul>
-          {products.length > 0 ? (
-            products.map((product: any, index) => (
-              <li data-testid="product" key={ index }>
-                {product.title}
-                <img src={ product.thumbnail } alt={ product.title } />
-                {product.results.price}
-              </li>
-            ))
-          ) : (
-            <p>Nenhum produto foi encontrado</p>
-          )}
-        </ul>
-      </section>
+      <Categorie />
     </>
   );
 }
-
 export default Home;
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,19 +2,17 @@ export async function getCategories() {
   const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(URL);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
+export async function getProductsFromCategoryAndQuery(categoryId: any, query: any) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-  const urlQuer = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  const urlCat = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-  const response = await fetch(urlQuer);
-  const result = await fetch(urlCat);
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const data = await response.json();
-  const dados = await result.json();
-  return data && dados;
+  console.log(data);
+
+  return data;
 }
 
 export async function getProductById() {
